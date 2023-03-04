94f0fff1f18e9cd3bdea091ae79cd375c1c8ed42
mohammad.sharifi <mohammad.sharifi@partsoftware.com>
2023-03-04 17:05:31 +0330

    implementing the basic server W/ putting the needed if/else and testing postman

--- server.js
+++ server.js
@@ -1,36 +1,63 @@
 const fs = require('fs/promises');
 const path = require('path');
 const redis = require('redis');
+const http = require('http');
 
 
-console.log('hello world');
 
 
 // یادبگیرم که طوری درخواست گت و پوت و پست بفرستم
 
-// wait till the request comes:
+var id ='';
+var data ='';
+var parent='';
+// wait for the requests to come:
+const server = http.createServer ( 
+    (request , respond) => {
+        if (request.url === 'dataService') {
 
-/*post:  recieves id, data, parent => store these in id-parent & id-data DB
-{
+            if (request.method == 'POST' ) {
+                /*post:  recieves id, data, parent => store these in id-parent & id-data DB
+                        a: if succeed:
+                        b: if ID was repetitive, "شناسه داده ها تکراری است"
+                        c: if REDIS thew any err, "خطای پایگاه داده"
+                */
 
-    a: if succeed:
-    b: if ID was repetitive, "شناسه داده ها تکراری است"
-    c: if REDIS thew any err, "خطای پایگاه داده"
-} */
+            } else if (request.method == 'GET') {
+                /* GET : recieves id & returns a json containing relative id-data-parent to the postman 
+                        a: if succeed, send the json
+                        b: if couldn't find the ID send "شناسه پیدا نشد"
+                        c: if REDIS threw any error, send "خطای پایگاه داده" to postman
+                */
 
-/* GET : recieves id & returns a json containing relative id-data-parent to the postman {
+            } else if (request.method == 'PUT') {
+                /* PUT : in order to editting, recieves a JSON from postman, and sends one of these
+                    a: if succeed: "داده ها بروز رسانی شد"
+                    b: if REDIS threw any error "خطای پایگاه داده"
+                    c: if USER/Postman sent any other kinda request, "درخواست معتبر نیست"
+                */
 
-    a: if succeed, send the json
-    b: if couldn't find the ID send "شناسه پیدا نشد"
-    c: if REDIS threw any error, send "خطای پایگاه داده" to postman
-} */
+            }
+        } 
+        // if (request.method == 'GET') {
+        //     console.log(request.url);
+        // }
+    }
+);
+server.listen ( 81 , () => {
+    console.log('server is listening'); 
+    // since we're using a callback function, it's an ASYNC request and won't stop application 
+})
 
-/* PUT : in order to editting, recieves a JSON from postman, and sends one of these {
-    
-    a: if succeed: "داده ها بروز رسانی شد"
-    b: if REDIS threw any error "خطای پایگاه داده"
-    c: if USER/Postman sent any other kinda request, "درخواست معتبر نیست"
-} */
+// the test message below is printed "before" the above ones. cuz app doesn't wait for upper async_functions to get finnished
+console.log('hello world');
+
+
+
+
+
+
+
 
 
 
@@ -56,9 +83,7 @@ db.on('error', err => {
         }
 ());
 
-var id ='';
-var data ='';
-var parent='';
+
 
 // write/import a new key-value to the DB
 db.set('forka', 'bar', (err, reply) => {
