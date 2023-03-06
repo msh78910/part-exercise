6754b1acb624fa79cb3dc734b7ac9f080d1e88fa
mohammad.sharifi <mohammad.sharifi@partsoftware.com>
2023-03-06 17:31:04 +0330

    adding router.js , requestHandler.js , index.js, as the book explained containing basic neccesary details in them

---
+++ requestHandler.js
@@ -0,0 +1,12 @@
+//page 30
+
+function start () {
+    console.log('request handler start was called');
+}
+
+function upload () {
+    console.log('request handler upload was called');
+}
+
+exports.start = start;
+exports.upload = upload;
---
+++ router.js
@@ -0,0 +1,5 @@
+function route (pathname) {
+    console.log('proccessing a route a request for ' + pathname);
+}
+
+exports.route = route;
