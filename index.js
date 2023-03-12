fa4a8719d48fdc90d1c71ae0cc5c7a8fe9126f19
mohammad.sharifi <mohammad.sharifi@partsoftware.com>
2023-03-12 14:54:57 +0330

    editting the index.js and madulizing the whole app

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const server = require('./server');
 const router = require('./router');
+const requestHandlers = require ("./requestHandlers");
 
-server.start (router.route);
+
+// herein we must introduce all kanda requests. but in the router app will decide which to handle
+var handle = {};
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/dataService"] = requestHandlers.dataService;
+
+server.start (router.route , handle);
