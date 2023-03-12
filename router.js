aa33818053409a3037895a3ac8c8e46a2a061369
mohammad.sharifi <mohammad.sharifi@partsoftware.com>
2023-03-12 14:57:30 +0330

    editting the router.js for madulizing the app

--- router.js
+++ router.js
@@ -1,5 +1,19 @@
-function route (pathname) {
-    console.log('proccessing a route a request for ' + pathname);
+// const requestHandlers = require('./requestHandlers');
+
+function route (handle, pathname, request, response) {
+    console.log('About to route a request for ' + pathname);
+    if (typeof handle[pathname] === 'function') { // checks it out if such a thing is a funciton
+
+        // if (request.url === 'dataService')
+        // ... 
+        handle[pathname](request, response);
+
+    } else {
+        console.log("No such request was found for " + pathname + ". There is no function to handle that")
+        response.writeHead(404, {'Content-Type' : "text/plain"});
+        response.write('404 Not found');
+        response.end();
+    }
 }
 
 exports.route = route;
